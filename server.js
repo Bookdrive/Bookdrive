b1ceac313c2734f2b5a4894d7049a065cbb5283a
amit0617 <71893015+Amit0617@users.noreply.github.com>
2022-02-09 22:06:19 +0530

    structure

---
+++ server.js
@@ -0,0 +1,10 @@
+'use strict'
+
+let express = require('express');
+let serveStatic = require('serve-static');
+
+let app = express();
+let port = 3000;
+
+app.use(serveStatic(path.join(__dirname, 'app')));
+app.listen(port, function(){console.log(`Listening on port ${port}`)});
